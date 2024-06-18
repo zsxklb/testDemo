f0048309f86a4f98d58ac3eccf7bfd5d383c7c0f
Hx <1359982062@qq.com>
2024-06-18 19:27:07 +0800

    1

--- src/main/java/cn/hzxy/Main.java
+++ src/main/java/cn/hzxy/Main.java
@@ -2,6 +2,6 @@ package cn.hzxy;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!this is a test!");
     }
 }
